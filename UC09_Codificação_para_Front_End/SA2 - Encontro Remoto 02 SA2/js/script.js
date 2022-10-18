43541568ea56062d133bab332d058058a6e2adea
Vander Michellao!git config --global user.name Vander <vander.michellao@gmail.com>
2022-10-18 09:48:46 -0300

    Atualizando funcionalidade password

--- UC09_Codificação_para_Front_End/SA2 - Encontro Remoto 02 SA2/js/script.js
+++ UC09_Codificação_para_Front_End/SA2 - Encontro Remoto 02 SA2/js/script.js
@@ -4,3 +4,13 @@ function cadastrarNewsletter() {
     alert(email)
     console.log(email)
 }
+
+function mouseoverPass(obj) {
+    var obj = document.getElementById('myPassword');
+    obj.type = "text";
+  }
+  function mouseoutPass(obj) {
+    var obj = document.getElementById('myPassword');
+    obj.type = "password";
+  }
+      
