8d4c3783588ae13499eb539f569fe0e7c63c522a
Vander Michellao!git config --global user.name Vander <vander.michellao@gmail.com>
2022-10-18 17:35:34 -0300

    Atualizando atividades!

---
+++ UC09_Codificação_para_Front_End/SA2 - Encontro Remoto 4/js/script.js
@@ -0,0 +1,16 @@
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+
+    alert(email)
+    console.log(email)
+}
+
+function mouseoverPass(obj) {
+    var obj = document.getElementById('myPassword');
+    obj.type = "text";
+  }
+  function mouseoutPass(obj) {
+    var obj = document.getElementById('myPassword');
+    obj.type = "password";
+  }
+      
