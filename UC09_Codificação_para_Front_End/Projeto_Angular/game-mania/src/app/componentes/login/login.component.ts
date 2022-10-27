7c7cdfd827e8cf66799343db8840a2cf3e629c02
Vander Michellao!git config --global user.name Vander <vander.michellao@gmail.com>
2022-10-27 16:41:56 -0300

    Atualizando o projeto

--- UC09_Codificação_para_Front_End/Projeto_Angular/game-mania/src/app/componentes/login/login.component.ts
+++ UC09_Codificação_para_Front_End/Projeto_Angular/game-mania/src/app/componentes/login/login.component.ts
@@ -9,12 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
  
   ngOnInit(): void {
-  }
-
-  
+  }  
 
   userModel = new User();
 
